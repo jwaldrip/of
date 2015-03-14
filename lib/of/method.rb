bdc573f6aebf6dd6582e672452534c82ec752833
Jason Waldrip <jason@waldrip.net>
2015-03-13 22:54:55 -0600

    support a class to instantiate n new of class

--- lib/of/method.rb
+++ lib/of/method.rb
@@ -1,6 +1,9 @@
 module Of
   METHOD = proc do
-    def of(&block)
+    def of(klass=nil, &block)
+      klass || block || raise(ArgumentError, 'must provide a `Class` or block')
+      block || klass.respond_to?(:new) || raise(ArgumentError, 'class must respond to `.new`')
+      block = proc { klass.new } if klass.is_a? Class
       self.times.map(&block)
     end
   end
--- spec/of_spec.rb
+++ spec/of_spec.rb
@@ -5,7 +5,19 @@ describe Of do
     expect(Of::VERSION).not_to be nil
   end
 
-  it 'gives n of something' do
+  it 'gives n of a block value' do
     expect(5.of { 'something' }).to eq(['something', 'something', 'something', 'something', 'something'])
+  end
+
+  it 'gives n of a Class that responds to new' do
+    expect(5.of(String)).to eq(['', '', '', '', ''])
+  end
+
+  it 'raises an ArgumentError when a class does not respond to new' do
+    expect { 5.of(Fixnum) }.to raise_error ArgumentError
+  end
+
+  it 'raises an ArgumentError when the instance is not a Class' do
+    expect { 5.of("a") }.to raise_error ArgumentError
   end
 end
